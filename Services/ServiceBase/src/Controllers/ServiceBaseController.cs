77f69933388dfa9e8008dd5824254573db098f7c
Stephen Laws <stephen.laws@nomostech.com>
2020-08-17 12:51:19 -0700

    Refactor and add auth

--- Services/ServiceBase/src/Controllers/ServiceBaseController.cs
+++ Services/ServiceBase/src/Controllers/ServiceBaseController.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //-------------------------------------------------------------------------
-namespace Health.PNET.Controllers
+namespace Health.PharmaNet.Controllers
 {
     using System.Collections.Generic;
     using System.Linq;
