ac8e761a3c341140200ea542eea46a736a05609f
bradhead <brad@zedwerks.com>
2020-10-16 13:48:30 -0700

    Added new services but need HL7v2 config

---
+++ Services/MedicationService/src/Controllers/MedicationController.cs
@@ -0,0 +1,79 @@
+﻿//-------------------------------------------------------------------------
+// Copyright © 2020 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
+namespace Health.PharmaNet.Controllers
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Health.PharmaNet.Common.Authorization.Policy;
+    using Health.PharmaNet.Common.Http;
+    using Health.PharmaNet.Parsers;
+    using Health.PharmaNet.Services;
+
+    using Hl7.Fhir.Model;
+
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// The MedicationService controller.
+    /// </summary>
+    [ApiVersion("1.0")]
+    [Route("/api/v{version:apiVersion}/Patient/")]
+    [ApiController]
+    public class MedicationController : ServiceBaseController
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MedicationController"/> class.
+        /// </summary>
+        /// <param name="logger">Injected Logger Provider.</param>
+        /// <param name="service">Injected service.</param>
+        /// <param name="parser">Injected HL7 message parser.</param>
+        /// <param name="configuration">Injected iConfiguration service.</param>
+        /// <param name="authorizationService">Injected authorization service.</param>
+        /// <param name="httpContextAccessor">The Http Context accessor.</param>
+        public MedicationController(
+            ILogger<ServiceBaseController> logger,
+            IPharmanetService service,
+            IHl7Parser parser,
+            IConfiguration configuration,
+            IAuthorizationService authorizationService,
+            IHttpContextAccessor httpContextAccessor)
+            : base(logger, service, parser, configuration, authorizationService, httpContextAccessor)
+        {
+        }
+
+        /// <summary>
+        /// This resource is primarily used for the identification and definition 
+        /// of a medication for the purposes of prescribing, dispensing, and administering
+        /// a medication as well as for making statements about medication use. </summary>
+        /// <returns>A DocumentReference response as Json.</returns>
+        /// <response code="200">Returns Ok when the transaction went through.</response>
+        /// <response code="401">Authorization error, returns JSON describing the error.</response>
+        [HttpPost]
+        [Produces("application/fhir+json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Authorize(Policy = FhirScopesPolicy.Access)]
+        public async Task<ActionResult<DocumentReference>> Medication()
+        {
+            return await base.PharmanetRequest().ConfigureAwait(false);
+        }
+    }
+}
