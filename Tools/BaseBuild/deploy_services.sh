2f12887dfaa099670e82dab9987799122083c3ff
Stephen Laws <stephen.laws@nomostech.com>
2020-10-20 14:53:55 -0700

    Deployment configs

--- Tools/BaseBuild/deploy_services.sh
+++ Tools/BaseBuild/deploy_services.sh
@@ -2,7 +2,14 @@
 
 # DEV
 echo "DEV started"
-./service.sh -s template -n fqlmjp-dev -e dev 
+./service.sh -s claimservice -n 2f77cb-dev -e dev 
+./service.sh -s locationservice -n 2f77cb-dev -e dev 
+./service.sh -s medicationdispenseservice -n 2f77cb-dev -e dev 
+./service.sh -s medicationrequestservice -n 2f77cb-dev -e dev 
+./service.sh -s medicationservice -n 2f77cb-dev -e dev 
+./service.sh -s medicationstatementservice -n 2f77cb-dev -e dev 
+./service.sh -s patientservice -n 2f77cb-dev -e dev 
+./service.sh -s practitionerservice -n 2f77cb-dev -e dev 
 echo "DEV ended"
 
 read -p "Deployment is DONE. Press [Enter] to exit..."
