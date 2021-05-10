7ec59ff6a24fd0aba2cc3edb62e9c10a8f282556
bradhead <brad@zedwerks.com>
2021-05-10 16:36:34 -0700

    added route to actions
    Signed-off-by: bradhead <brad@zedwerks.com>

--- Services/ConsentService/src/Controllers/ConsentController.cs
+++ Services/ConsentService/src/Controllers/ConsentController.cs
@@ -68,6 +68,8 @@ namespace Health.PharmaNet.Controllers
         /// <response code="200">Returns Ok when the transaction went through.</response>
         /// <response code="401">Authorization error, returns JSON describing the error.</response>
         [HttpPost]
+        [Route("")]
+        [Route("/api/v{version:apiVersion}/Consent/")]
         [Produces("application/fhir+json")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [Authorize(Policy = FhirScopesPolicy.Access)]
