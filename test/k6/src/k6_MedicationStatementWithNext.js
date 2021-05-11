f7faa4a659f7f8c5de0c0418b3b54f052eb6b0c8
bradhead <brad@zedwerks.com>
2021-05-11 15:52:33 -0700

    Add NEXT Example to k6
    Signed-off-by: bradhead <brad@zedwerks.com>

---
+++ test/k6/src/k6_MedicationStatementWithNext.js
@@ -0,0 +1,30 @@
+//-------------------------------------------------------------------------
+// Copyright © 2021 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
+
+import { sleep } from 'k6';
+import * as common from './inc/common.js';
+import * as examples from './inc/examples/MedicationStatement.js';
+
+export default function() {
+
+    var url = common.MedicationStatementServiceUrl;
+    var scopes = "openid audience system/MedicationStatement.read";
+    common.authorizeClient(scopes);
+    common.submitMessage(url, examples.MedicationStatement[1]);  // Original Request
+    sleep(1);
+    common.submitMessage(url, examples.MedicationStatement[2]); // The NEXT Example
+
+}
