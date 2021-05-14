76dbfc64653b36e008f4c0309cd85c098fbeb9cc
bradhead <brad@zedwerks.com>
2021-05-13 17:14:21 -0700

    update configs to match docs. update samples
    Signed-off-by: bradhead <brad@zedwerks.com>

--- test/k6/src/inc/examples/MedicationStatement.js
+++ test/k6/src/inc/examples/MedicationStatement.js
@@ -15,6 +15,61 @@
 //-------------------------------------------------------------------------
 export let MedicationStatement = [
     {
+        name: "TDU_00_REQUEST - 674212",
+        purpose: "DUE Inquiry request message",
+        version: "PNet-R3",
+        message:
+            "MSH|^~\\&|DESKTOP|EMR|DESKTOP|EMRMD|${{ timestamp }}|userID:192.168.0.1|ZPN|674212|P|2.1||\r" +
+            "ZCA|1|70|00|MA|01|\r" +
+            "ZCB|LDJQQ|140715|674212\r" +
+            "ZCC||||||||LYGYD|PQLZVK|000nnnnnnnnnn|M\r" +
+            "ZCD||||319233|||66999997||100|25|||||||||||||||\r" +
+            "ZPJ|ZPJ1^^^^^^|ZPJ2^^~ZPJ2^^~ZPJ2^^|ZPJ3^^|ZPJ4^Transaction Trace ID: 674212\r" +
+            "ZZZ|TDU||674212|P1|nnnnnnnnnn|||||ZZZ1^\r"
+    },
+    {
+        name: "TDUTRP_00_REQUEST-332212",
+        purpose: "DUE Inquiry TDU/TRP request message",
+        version: "PNet-R3",
+        message:
+            "MSH|^~\\&|1234567|1234567||ERXPP|${{ timestamp }}|userID:192.168.0.1|ZPN|332212|P|2.1||\r" +
+            "ZZZ|TDU||332212|P1|nnnnnnnnnn||||\r" +
+            "ZZZ|TRP||332212|P1|nnnnnnnnnn||||\r" +
+            "ZCA|1|70|00|AR|05|\r" +
+            "ZCB|BC00007310|140912|332212\r" +
+            "ZCC||||||||||000nnnnnnnnnn|\r" +
+            "ZCD||||319233|||2326450||||||||||||||||||87168\r" +
+            "ZPJ|ZPJ1^^^^^^|ZPJ2^^~ZPJ2^^~ZPJ2^^|ZPJ3^^|ZPJ4^\r"
+    },
+    {
+        name: "TDUTRR_00_REQUEST-332214",
+        purpose: "DUE Inquiry TDU/TRR request message",
+        version: "PNet-R3",
+        message:
+            "MSH|^~\\&|1234567|1234567||ERXPP||userID:192.168.0.1|ZPN|332214|P|2.1||\r" +
+            "ZZZ|TRR||332214|P1|nnnnnnnnnn||||\r" +
+            "ZZZ|TDU||332214|P1|nnnnnnnnnn||||\r" +
+            "ZCA|1|70|00|AR|05|\r" +
+            "ZCB|BC00007310|140912|332214\r" +
+            "ZCC||||||||||000nnnnnnnnnn|\r" +
+            "ZCD||||319233|||66123264||||||||||||||||||2915\r" +
+            "ZPJ|ZPJ1^^^^^^|ZPJ2^^~ZPJ2^^~ZPJ2^^|ZPJ3^^|ZPJ4^\r"
+    },
+    {
+        name: "TDUTRS_00_REQUEST-332251",
+        purpose: "DUE Inquiry TDU/TRS request message",
+        version: "PNet-R3",
+        message:
+            "MSH|^~\\&|1234567|1234567||ERXPP||userID:192.168.0.1|ZPN|332251|P|2.1||\r" +
+            "ZZZ|TRS||332251|P1|nnnnnnnnnn||||\r" +
+            "ZZZ|TDU||332251|P1|nnnnnnnnnn||||\r" +
+            "ZCA|1|70|00|AR|05|\r" +
+            "ZCB|BC00007310|141012|332251\r" +
+            "ZCC||||||||||000nnnnnnnnnn|\r" +
+            "ZCD||||319233|||2063735||||||||||||||||||912351177\r" +
+            "ZPJ|ZPJ1^^^^^^|ZPJ2^^~ZPJ2^^~ZPJ2^^|ZPJ3^^|ZPJ4^\r"
+    },
+    {
         name: "TRP_00_REQUEST - R3",
         purpose: "",
         version: "PNet-R3",
@@ -83,6 +138,17 @@ export let MedicationStatement = [
             "ZZZ|TRP||613252|P1|nnnnnnnnnn||||\r" +
             "ZCA||70|00|MA|01|\r" +
             "ZCB|LDJQQ|140806|613252\r" +
+            "ZCC||||||||||000nnnnnnnnnn|\r\r"
+    },
+    {
+        name: "TRR_00_REQUEST-235348",
+        purpose: "Patient Profile Most Recent Only Request message",
+        version: "PNet-R70",
+        message:
+            "MSH|^~\\&|1234567|1234567||ERXPP|${{ timestamp }}|userID:192.168.0.1|ZPN|235348|P|2.1||\r" +
+            "ZZZ|TRR||235348|P1|nnnnnnnnnn|||||ZZZ1^\r" +
+            "ZCA||70|00|KC|13|\r" +
+            "ZCB|BYR|111006|235348\r" +
             "ZCC||||||||||000nnnnnnnnnn|\r\r"
     }
 ];
