132c9ba965c59b160bb3b2ca61985577c2ee36b2
Arlo Watts <67204708+arlowatts@users.noreply.github.com>
2023-07-21 11:58:44 -0700

    Update to use automatic BASEDIR

--- test/k6/inc/run-all.sh
+++ test/k6/inc/run-all.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#!/bin/bash
 # Runs a simple smoke test on all services based on the variables set below
 
 # Set env to be the three-letter name of the environment (such as dev, vs1, or vc1)
@@ -12,24 +12,23 @@ client=
 # Set secret to be the client secret associated with the client id in keycloak
 secret=
 
-# Set homedir to the local path containing the repo
-# The output of the tests is saved to {homedir}/test/k6/output/{env}
-homedir=
+# Points to test/k6
+BASEDIR=$(dirname $0)/..
 
 services=('Claim' 'Consent' 'Location' 'Medication' 'MedicationDispense' 'MedicationRequest' 'MedicationStatement' 'Patient' 'Practitioner')
 
-mkdir ${homedir}/test/k6/output/${env}
+mkdir ${BASEDIR}/output/${env}
 
-for i in "${!services[@]}"; do
-  echo Testing ${services[$i]}Service...
+for service in "${services[@]}"; do
+  echo Testing ${service}Service...
 
   # Set the env variables for the command
   # Compose up the test container
   # Write the output to a file
   ERX_ENV=${env} ERX_CLIENT=${client} ERX_CLIENT_SECRET=${secret} \
-  docker compose --file ${homedir}/test/k6/api/${services[$i]}/docker-compose.yml up \
-  > ${homedir}/test/k6/output/${env}/k6-${env}-${services[$i]}.txt 2>&1
+  docker compose --file ${BASEDIR}/api/${service}/docker-compose.yml up \
+  > ${BASEDIR}/output/${env}/k6-${env}-${service}.txt 2>&1
 
   # Quietly delete the compose stack
-  docker compose --file ${homedir}/test/k6/api/${services[$i]}/docker-compose.yml rm --force > /dev/null 2>&1
+  docker compose --file ${BASEDIR}/api/${service}/docker-compose.yml rm --force > /dev/null 2>&1
 done
