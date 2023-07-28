44c504db79ff9d371568c0b7848d096a9a243576
arlowatts <arlowatts1@gmail.com>
2023-07-28 10:50:05 -0700

    Update test framework to use env-specific data

---
+++ test/k6/inc/examples/vc1/Medication.js
@@ -0,0 +1,55 @@
+//-------------------------------------------------------------------------
+// Copyright © 2021 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
+
+export const examples = [
+
+    {
+        name: "TDR_50_REQUEST - R3",
+        purpose: "Drug Monograph Information request message",
+        version: "PNet-R3",
+        message:
+            "MSH|^~\\&|DESKTOP|BC01400503|PNP|IP||DRB:216.232.132.137|ZPN|103849|P|2.1\r" +
+        "ZZZ|TDR||103849|P1|12345|||\r" +
+        "ZCA|000001|70|00|AR|07\r" +
+        "ZCB|BC00000ZZZ|210603|103849\r" +
+        "ZPC|337730||||||N|ZPC1^FDB^EDUCLONG\r"
+    },
+
+    {
+        name: "TDR_50_REQUEST - R3",
+        purpose: "Drug Monograph Information request message",
+        version: "PNet-R3",
+        message:
+            "MSH|^~\\&|TRXTOOL|PCARESUP|PNP|PP|${{ timestamp }}||ZPN|9286|P|2.1||\r" +
+            "ZZZ|TDR||9286|P1|2F3P2||||\r" +
+            "ZCA||03|00|KC|13|\r" +
+            "ZCB|BC00007007|201222|9286\r" +
+            "ZPC|2240579||||||Y|ZPC1^^^766720\r\r"
+
+    },
+    {
+        name: "TDR_50_REQUEST - 149966",
+        purpose: "Drug Monograph Information request message",
+        version: "PNet-R70",
+        includeIdentifier: false,
+        message:
+            "MSH|^~\\&|1234567|1234567||ERXPP|${{ timestamp }}|userID:192.168.0.1|ZPN|149966|P|2.1||\r" +
+            "ZZZ|TDR||149966|P1|nnnnnnnnnn|||||ZZZ1^\r" +
+            "ZCA||70|00|KC|13|\r" +
+            "ZCB|BC00000F07|111205|149966\r" +
+            "ZPC|122858||||||N|ZPC1^FDB^ADIMONOG^0\r"
+    }
+];
---
+++ test/k6/run-all.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Runs a simple smoke test on all services based on the five variables set below
+# Runs all services simultaneously and then captures their logs one by one as they finish
+
+# Set env to be the three-letter name of the environment (such as dev, vs1, or vc1)
+# The tests invoke the APIs at https://pnet-{env}.api.gov.bc.ca
+# env must be set - it cannot be left blank
+env=
+
+# Set client to be the keycloak client id, like erx_development or ppm_development
+# The dev environment requires the erx_development client, all others require ppm_development
+# Defaults to erx_development
+client=
+
+# Set secret to be the client secret associated with the client id in keycloak
+# secret must be set - it cannot be left blank
+secret=
+
+# Set vus to the desired number of maximum concurrent users
+# See https://github.com/bcgov/moh-eRx/wiki/k6-Testing#virtual-users-vus for more info about virtual users
+# Defaults to 1 - for a smoke test, 1 vu is sufficient
+vus=
+
+# Set iterations to the total desired number of iterations of the test scripts
+# The number of iterations is independent of the number of virtual users
+# Defaults to 1 - for a smoke test, 1 iteration is sufficient
+iterations=
+
+# Points to test/k6
+BASEDIR=$(dirname $0)
+
+services=('Claim' 'Consent' 'Location' 'Medication' 'MedicationDispense' 'MedicationRequest' 'MedicationStatement' 'Patient' 'Practitioner')
+
+mkdir ${BASEDIR}/output/${env}
+
+# Set environment variables for docker compose stacks
+export ERX_ENV=${env} ERX_CLIENT=${client} ERX_CLIENT_SECRET=${secret} ERX_VUS=${vus} ERX_ITERATIONS=${iterations}
+
+for service in "${services[@]}"; do
+  # Compose up the test container in the background - this will run all nine containers concurrently
+  # Runs the container from test/k6/api/docker-componse.yml with a lowercase name
+  ERX_SERVICE=${service} \
+  docker compose --project-name $(echo ${service} | tr '[:upper:]' '[:lower:]') \
+                 --file ${BASEDIR}/api/docker-compose.yml \
+                 up --detach
+done
+
+for service in "${services[@]}"; do
+  echo Logging ${service}Service...
+
+  # Capture the logs of the container - logs are not read concurrently, but sequentially
+  docker compose --project-name $(echo ${service} | tr '[:upper:]' '[:lower:]') \
+                 logs --follow > ${BASEDIR}/output/${env}/k6-${env}-${service}.txt
+
+  # Delete the compose stack after the logs are finished
+  docker compose --project-name $(echo ${service} | tr '[:upper:]' '[:lower:]') \
+                 rm --force
+done
