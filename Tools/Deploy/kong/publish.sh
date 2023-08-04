5414ef8e721022dd474b85043cfb0de8a70c49c6
arlowatts <arlowatts1@gmail.com>
2023-08-04 13:47:10 -0700

    Remove unused kong files

--- Tools/Deploy/kong/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-environments=('dev' 'test' 'prod')
-environments=('dev')
-for environment in "${environments[@]}"; do
-    export $(grep -v '^#' $environment-pnet.env | xargs)
-    ./gwa pg "output/$environment-pnet.yaml"
-done
