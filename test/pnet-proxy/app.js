8ad28a80b15bc3ad5cff801aa0179a6cbb5ca2fb
arlowatts <arlowatts1@gmail.com>
2023-11-07 10:18:13 -0800

    Clean up

--- test/pnet-proxy/app.js
+++ test/pnet-proxy/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const port = 8080;
 const host = "0.0.0.0";
@@ -9,8 +8,6 @@ const responseJson = {
     "transactionUUID": "22c263c9-1903-4cbf-8eb2-c43fa7b9dfa7",
     "hl7Message": "TVNIfF5+XCZ8MTIzNDU2N3wxMjM0NTY3fDEyMzQ1Njd8MTIzNDU2N3x8fFpQTnw0NDM3Njl8UHwyLjF8ClpDQnxCQzAwMDAwSTEwfDE0MDcxNXw0NDM3NjkKWlpafFRJRHwwfDQ0Mzc2OXxQMXxubm5ubm5ubm5ufHwwIE9wZXJhdGlvbiBzdWNjZXNzZnVsfHx8WlpaMQpaQ0N8fHx8fDE5NTAwMjE3fHx8fHwwMDBubm5ubm5ubm5ufEYKWlBBfE5TTkRKREdaVHxaTUFYQXxXfFpQQTFeXl42MDReNDU2MzE2N3xaUEEyXk1eXl5eXlhWWVRORERDT09HXl5TVVJSRVleQ0FOXlYyUjdFMF5eQkNeXl5eXl5eXl4="
 };
-
-app.use(bodyParser.json());
 
 app.post("/submit", function (req, res) {
     console.log("http://" + host + ":" + port + "/submit: POST");
