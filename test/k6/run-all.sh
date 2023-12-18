bbfc4182c4ee6a07af80758743c3d73f2fdda0b5
arlowatts <arlowatts1@gmail.com>
2023-12-18 12:35:30 -0800

    Update test script to print usage

--- test/k6/run-all.sh
+++ test/k6/run-all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Runs a smoke test (or load test) on all PPM API services and logs the results in output/<ENVIRONMENT>
+# Runs a smoke test (or load test) on all PPM API services and logs the results in output/$ENVIRONMENT
 #
 # Usage:
-#   run-all.sh ENVIRONMENT CLIENT_ID CLIENT_SECRET VUS ITERATIONS ITERATION_LENGTH
+#   run-all.sh ENVIRONMENT CLIENT_ID CLIENT_SECRET [VUS] [ITERATIONS] [ITERATION_LENGTH]
 #
 # Substitute ENVIRONMENT for the three-letter name of the PPM API environment to be tested. The environments are dev, tr1, vs1, vc1, vc2, and prd.
 # Substitute CLIENT_ID for the Keycloak client ID associated with the given environment.
-# Substitute CLIENT_SECRET for the client secret associated with the given client ID.
+# Substitute CLIENT_SECRET for the client secret associated with the given client ID. Set to "stdin" to read the secret from standard input rather than command line arguments.
 # Substitute VUS for the maximum number of concurrent virtual users. Defaults to 1 if not set. See the confluence documentation for more information about virtual users.
 # Substitute ITERATIONS for the number of iterations to run on each service. Defaults to 1 if not set.
 # Substitute ITERATION_LENGTH for the number of transactions sent in each iteration. Set to -1 to run all available test transactions on every service. Defaults to -1 if not set.
@@ -21,6 +21,24 @@ secret=$3
 vus=$4
 iterations=$5
 iterationLength=$6
+
+if [ "$secret" = "" ]; then
+  echo "Usage:"
+  echo "  run-all.sh ENVIRONMENT CLIENT_ID CLIENT_SECRET [VUS] [ITERATIONS] [ITERATION_LENGTH]"
+  echo
+  echo "Substitute ENVIRONMENT for the three-letter name of the PPM API environment to be tested. The environments are dev, tr1, vs1, vc1, vc2, and prd."
+  echo "Substitute CLIENT_ID for the Keycloak client ID associated with the given environment."
+  echo "Substitute CLIENT_SECRET for the client secret associated with the given client ID. Set to "stdin" to read the secret from standard input rather than command line arguments."
+  echo "Substitute VUS for the maximum number of concurrent virtual users. Defaults to 1 if not set. See the confluence documentation for more information about virtual users."
+  echo "Substitute ITERATIONS for the number of iterations to run on each service. Defaults to 1 if not set."
+  echo "Substitute ITERATION_LENGTH for the number of transactions sent in each iteration. Set to -1 to run all available test transactions on every service. Defaults to -1 if not set."
+  exit 1
+
+elif [ "$secret" = "stdin" ]; then
+  echo "Enter the client secret: "
+  read secret
+
+fi
 
 # To test fewer services with this script, remove them from this list
 services=('Claim' 'Consent' 'Location' 'Medication' 'MedicationDispense' 'MedicationRequest' 'MedicationStatement' 'Patient' 'Practitioner')
