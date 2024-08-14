3c59f3056d1ea247508a6ed4e95c950d0d8a2c26
Arlo Watts <arlowatts1@gmail.com>
2024-08-14 11:55:50 -0700

    Change service names to lowercase to fix docker compose

--- test/k6/common.js
+++ test/k6/common.js
@@ -23,42 +23,60 @@ import { meanDelaySeconds } from "./options.js";
 
 export { options } from "./options.js";
 
+// dictionary to select the correct base api url by environment
+const environmentBaseApiUrls = {
+    "dev": "https://pnet-dev.api.gov.bc.ca/api/v1/",
+    "vs1": "https://pnet-vs1.api.gov.bc.ca/api/v1/",
+    "tr1": "https://pnet-tr1.api.gov.bc.ca/api/v1/",
+    "vc2": "https://pnet-vc2.api.gov.bc.ca/api/v1/",
+    "vc1": "https://pnet-vc1.api.gov.bc.ca/api/v1/",
+    "prd": "https://pnet.api.gov.bc.ca/api/v1/",
+};
+
+// dictionary to select the correct keycloak token url by environment
+const environmentTokenUrls = {
+    "dev": "https://common-logon-dev.hlth.gov.bc.ca/auth/realms/v2_pos/protocol/openid-connect/token",
+    "vs1": "https://common-logon-test.hlth.gov.bc.ca/auth/realms/moh_applications/protocol/openid-connect/token",
+    "tr1": "https://common-logon-test.hlth.gov.bc.ca/auth/realms/moh_applications/protocol/openid-connect/token",
+    "vc2": "https://common-logon-test.hlth.gov.bc.ca/auth/realms/moh_applications/protocol/openid-connect/token",
+    "vc1": "https://common-logon-test.hlth.gov.bc.ca/auth/realms/moh_applications/protocol/openid-connect/token",
+    "prd": "https://common-logon.hlth.gov.bc.ca/auth/realms/moh_applications/protocol/openid-connect/token",
+};
+
+// dictionary to capitalize the service name by service
+const serviceNames = {
+    "claim": "Claim",
+    "consent": "Consent",
+    "location": "Location",
+    "medication": "Medication",
+    "medicationdispense": "MedicationDispense",
+    "medicationrequest": "MedicationRequest",
+    "medicationstatement": "MedicationStatement",
+    "patient": "Patient",
+    "practitioner": "Practitioner",
+};
+
+// dictionary to select the keycloak scopes by service
+const serviceScopes = {
+    "claim": "openid system/Claim.write system/Claim.read",
+    "consent": "openid system/Patient.read system/Consent.write system/Consent.read",
+    "location": "openid system/Location.read",
+    "medication": "openid system/Medication.read",
+    "medicationdispense": "openid system/MedicationDispense.write system/MedicationDispense.read",
+    "medicationrequest": "openid system/MedicationRequest.write system/MedicationRequest.read",
+    "medicationstatement": "openid system/MedicationStatement.read",
+    "patient": "openid system/Patient.read system/Patient.write",
+    "practitioner": "openid system/Practitioner.read",
+};
+
 // environment and service are defined by environment variables set when the k6
 // command to execute the test is run
 const environment = __ENV.ERX_ENV;
 const service = __ENV.ERX_SERVICE;
 const iterationLength = __ENV.ERX_ITERATION_LENGTH ? __ENV.ERX_ITERATION_LENGTH : 1;
 
-// the services follow a common naming scheme by environment, except prd which
-// uses the plain url
-const baseUrl = environment === "prd" ? "https://pnet.api.gov.bc.ca/api/v1/" : "https://pnet-" + environment + ".api.gov.bc.ca/api/v1/";
-
-const serviceUrl = baseUrl + service;
-
-// the keycloak url that will return the access token depends on the environment
-let tokenUrl = "";
-
-if (environment === "prd") {
-    tokenUrl = "https://common-logon.hlth.gov.bc.ca/auth/realms/moh_applications/protocol/openid-connect/token";
-} else if (environment === "dev") {
-    tokenUrl = "https://common-logon-dev.hlth.gov.bc.ca/auth/realms/v2_pos/protocol/openid-connect/token";
-} else {
-    tokenUrl = "https://common-logon-test.hlth.gov.bc.ca/auth/realms/moh_applications/protocol/openid-connect/token";
-}
-
-// each service requires different scopes from its users
-// the test framework uses these with its token requests to keycloak
-const scopes = {
-    "Claim": "openid system/Claim.write system/Claim.read",
-    "Consent": "openid system/Patient.read system/Consent.write system/Consent.read",
-    "Location": "openid system/Location.read",
-    "Medication": "openid system/Medication.read",
-    "MedicationDispense": "openid system/MedicationDispense.write system/MedicationDispense.read",
-    "MedicationRequest": "openid system/MedicationRequest.write system/MedicationRequest.read",
-    "MedicationStatement": "openid system/MedicationStatement.read",
-    "Patient": "openid system/Patient.read system/Patient.write",
-    "Practitioner": "openid system/Practitioner.read"
-};
+const serviceUrl = environmentBaseApiUrls[environment] + serviceNames[service];
+const tokenUrl = environmentTokenUrls[environment];
 
 // the client object stores information about the simulated client
 const client = {
@@ -66,7 +84,7 @@ const client = {
     secret: __ENV.ERX_CLIENT_SECRET,
     token: null,
     expires: null,
-    scopes: scopes[service],
+    scopes: serviceScopes[service],
 };
 
 // k6 runs this function once for each iteration
--- test/k6/run-all.sh
+++ test/k6/run-all.sh
@@ -13,7 +13,7 @@
 # Substitute ITERATION_LENGTH for the number of transactions sent in each iteration. Set to -1 to run all available test transactions on every service. Defaults to 1 if not set.
 #
 # Author: Arlo Watts
-# Date: 2023-30-11
+# Date: 2024-08-14
 
 env=$1
 client=$2
@@ -42,7 +42,7 @@ elif [ "$secret" = "stdin" ]; then
 fi
 
 # To test fewer services with this script, remove them from this list
-services=('Claim' 'Consent' 'Location' 'Medication' 'MedicationDispense' 'MedicationRequest' 'MedicationStatement' 'Patient' 'Practitioner')
+services=('claim' 'consent' 'location' 'medication' 'medicationdispense' 'medicationrequest' 'medicationstatement' 'patient' 'practitioner')
 
 BASEDIR=$(dirname $0) # Points to test/k6
 mkdir --parents ${BASEDIR}/output/${env}
@@ -52,24 +52,19 @@ export ERX_ENV=${env} ERX_CLIENT=${client} ERX_CLIENT_SECRET=${secret} ERX_VUS=$
 
 for service in "${services[@]}"; do
   # Compose up the test container in the background - this will run all nine containers concurrently
-  # Runs the container from test/k6/api/docker-componse.yml with a lowercase name
-  ERX_SERVICE=${service} \
-  docker compose --project-name $(echo ${service} | tr '[:upper:]' '[:lower:]') \
-                 --file ${BASEDIR}/docker-compose.yml \
-                 up --detach
+  # Runs the container from test/k6/api/docker-componse.yml
+  ERX_SERVICE=${service} docker compose --project-name ${service} --file ${BASEDIR}/docker-compose.yml up --detach
 done
 
 for service in "${services[@]}"; do
-  echo Logging ${service}Service to ${BASEDIR}/output/${env}/k6-${env}-${service}.txt
+  echo Logging ${service}Service to ${BASEDIR}/output/${env}/${service}.txt
 
   # Capture the logs of the container - logs are not read concurrently, but sequentially
-  docker compose --project-name $(echo ${service} | tr '[:upper:]' '[:lower:]') \
-                 logs --follow > ${BASEDIR}/output/${env}/${service}.txt
+  docker compose --project-name ${service} logs --follow > ${BASEDIR}/output/${env}/${service}.txt
 
   # Delete the compose stack after the logs are finished
-  docker compose --project-name $(echo ${service} | tr '[:upper:]' '[:lower:]') \
-                 rm --force
+  docker compose --project-name ${service} rm --force
 done
 
 # Grab the summaries from each log file and dump them in a new file
-tail -n 24 ${BASEDIR}/output/${env}/* > ${BASEDIR}/output/${env}/summary.txt
+tail --lines 24 ${BASEDIR}/output/${env}/* > ${BASEDIR}/output/${env}/summary.txt
