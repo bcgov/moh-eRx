49258eaef677feaac7823c4fc30a164f8ebc8c87
Arlo Watts <arlowatts1@gmail.com>
2024-08-28 12:46:32 -0700

    Add maxDuration configuration option

--- test/k6/run-all.sh
+++ test/k6/run-all.sh
@@ -3,7 +3,7 @@
 # Runs a smoke test (or load test) on all PPM API services and logs the results in output/$ENVIRONMENT
 #
 # Usage:
-#   run-all.sh ENVIRONMENT CLIENT_ID CLIENT_SECRET [VUS] [ITERATIONS] [ITERATION_LENGTH]
+#   run-all.sh ENVIRONMENT CLIENT_ID CLIENT_SECRET [VUS] [ITERATIONS] [ITERATION_LENGTH] [MAX_DURATION]
 #
 # Substitute ENVIRONMENT for the three-letter name of the PPM API environment to be tested. The environments are dev, tr1, vs1, vc1, vc2, and prd.
 # Substitute CLIENT_ID for the Keycloak client ID associated with the given environment.
@@ -11,6 +11,7 @@
 # Substitute VUS for the maximum number of concurrent virtual users. Defaults to 1 if not set. See the confluence documentation for more information about virtual users.
 # Substitute ITERATIONS for the number of iterations to run on each service. Defaults to 1 if not set.
 # Substitute ITERATION_LENGTH for the number of transactions sent in each iteration. Set to -1 to run all available test transactions on every service. Defaults to 1 if not set.
+# Substitute MAX_DURATION for the maximum duration of the test. Defaults to '10m' (10 minutes) if not set.
 #
 # Author: Arlo Watts
 # Date: 2024-08-14
@@ -21,10 +22,11 @@ secret=$3
 vus=$4
 iterations=$5
 iterationLength=$6
+maxDuration=$7
 
 if [ "$secret" = "" ]; then
   echo "Usage:"
-  echo "  run-all.sh ENVIRONMENT CLIENT_ID CLIENT_SECRET [VUS] [ITERATIONS] [ITERATION_LENGTH]"
+  echo "  run-all.sh ENVIRONMENT CLIENT_ID CLIENT_SECRET [VUS] [ITERATIONS] [ITERATION_LENGTH] [MAX_DURATION]"
   echo
   echo "Substitute ENVIRONMENT for the three-letter name of the PPM API environment to be tested. The environments are dev, tr1, vs1, vc1, vc2, and prd."
   echo "Substitute CLIENT_ID for the Keycloak client ID associated with the given environment."
@@ -32,6 +34,7 @@ if [ "$secret" = "" ]; then
   echo "Substitute VUS for the maximum number of concurrent virtual users. Defaults to 1 if not set. See the confluence documentation for more information about virtual users."
   echo "Substitute ITERATIONS for the number of iterations to run on each service. Defaults to 1 if not set."
   echo "Substitute ITERATION_LENGTH for the number of transactions sent in each iteration. Set to -1 to run all available test transactions on every service. Defaults to 1 if not set."
+  echo "Substitute MAX_DURATION for the maximum duration of the test. Defaults to '10m' (10 minutes) if not set."
   exit 1
 
 elif [ "$secret" = "stdin" ]; then
@@ -51,7 +54,7 @@ mkdir --parents ${BASEDIR}/output/${env}
 rm --force ${BASEDIR}/output/${env}/*
 
 # Set environment variables for docker compose stacks
-export ERX_ENV=${env} ERX_CLIENT=${client} ERX_CLIENT_SECRET=${secret} ERX_VUS=${vus} ERX_ITERATIONS=${iterations} ERX_ITERATION_LENGTH=${iterationLength}
+export ERX_ENV=${env} ERX_CLIENT=${client} ERX_CLIENT_SECRET=${secret} ERX_VUS=${vus} ERX_ITERATIONS=${iterations} ERX_ITERATION_LENGTH=${iterationLength} ERX_MAX_DURATION=${maxDuration}
 
 for service in "${services[@]}"; do
   # Compose up the test container in the background - this will run all nine containers concurrently
