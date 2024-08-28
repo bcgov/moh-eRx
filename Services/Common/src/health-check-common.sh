b6475422e9ca7ab0cd95b9dc4164285b27ed3a7d
Arlo Watts <arlowatts1@gmail.com>
2024-08-27 18:14:30 -0700

    Remove jq dependency

--- Services/Common/src/health-check-common.sh
+++ Services/Common/src/health-check-common.sh
@@ -33,10 +33,10 @@ accessToken=$(curl --location --request POST ${tokenEndpoint} \
   --data-urlencode "client_id=${HEALTH_CHECK_CLIENT_ID}" \
   --data-urlencode 'audience=pharmanet' \
   --data-urlencode 'scope=openid system/*.write system/*.read system/Claim.read system/Claim.write system/Consent.read system/Consent.write system/Location.read system/Medication.read system/MedicationDispense.read system/MedicationDispense.write system/MedicationRequest.read system/MedicationRequest.write system/MedicationStatement.read system/Patient.read system/Patient.write system/Practitioner.read' \
-  --data-urlencode "client_secret=${HEALTH_CHECK_CLIENT_SECRET}" 2> /dev/null | jq -r '.access_token')
+  --data-urlencode "client_secret=${HEALTH_CHECK_CLIENT_SECRET}" 2> /dev/null | sed 's/.*"access_token":"\([0-9a-zA-Z_\-]*\.[0-9a-zA-Z_\-]*\.[0-9a-zA-Z_\-]*\)".*/\1/')
 
 # submit a transaction to the current service
-curl --request POST 'https://127.0.0.1:8080/' \
+curl --silent --request POST 'https://127.0.0.1:8080/' \
   --header 'Content-Type: application/json' \
   --header "Authorization: Bearer ${accessToken}" \
-  --data "{'resourceType':'DocumentReference','status':'current','date':'$(date --iso-8601=seconds)','content':[{'attachment':{'contentType':'x-application/hl7-v2+er7','data':'${payload}'}}]}" > /dev/null
+  --data "{'resourceType':'DocumentReference','status':'current','date':'$(date --iso-8601=seconds)','content':[{'attachment':{'contentType':'x-application/hl7-v2+er7','data':'${payload}'}}]}"
